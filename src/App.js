90b2e4fbad946b220e0fd975868c90ae20ddb27c
RedaDevJs <reda.elkhechabi@gmail.com>
2024-02-28 01:02:03 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout/Layout";
+import React, {Fragment} from "react";
+import Header from "./components/Header/Header";
+import Routers from "./routers/Routers";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Fragment>
+            <Header />
+            <div>
+                <Routers />
+            </div>
+            <Footer />
+        </Fragment>
+    );
 }
-
-export default App;
