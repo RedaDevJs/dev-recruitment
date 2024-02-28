44d4e258cf05463a70b347de5cdfbe7b65c2802d
RedaDevJs <reda.elkhechabi@gmail.com>
2024-02-28 01:06:05 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
 import Layout from "./components/Layout/Layout";
-import React, {Fragment} from "react";
-import Header from "./components/Header/Header";
-import Routers from "./routers/Routers";
-import Footer from "./components/Footer/Footer";
 
 export default function App() {
-    return (
-        <Fragment>
-            <Header />
-            <div>
-                <Routers />
-            </div>
-            <Footer />
-        </Fragment>
-    );
+    return (<Layout/>);
 }
